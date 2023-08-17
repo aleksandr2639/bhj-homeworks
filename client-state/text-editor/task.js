1f5483c53484916733c5417ddeb3531adf2ad3f8
aleksandr2639 <alexanderkriulin@yandex.ru>
2023-08-17 21:34:56 +0300

    add: text-editor/task.js

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,15 @@
+const textEditor = document.querySelector('#editor')
+const buttonClear = document.querySelector('.btn')
+window.addEventListener('load', () => {
+    textEditor.addEventListener('input', () => {
+        localStorage.setItem('text', textEditor.value);
+    })
+
+    if (localStorage.text) {
+        textEditor.value = localStorage.getItem('text');
+    }
+})
+buttonClear.addEventListener('click', () => {
+    localStorage.removeItem('text');
+    textEditor.value ='';
+})
